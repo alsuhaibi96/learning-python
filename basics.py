3d6658bf84e782624dac97262e5958f884af2a23
Abdulrahman Alsuhaibi <109905084+a-alsuhaibi@users.noreply.github.com>
2023-01-29 10:24:00 +0300

    learning-python-basics

---
+++ basics.py
@@ -0,0 +1,15 @@
+"""if 5>2 :
+ print("five is greater than two")
+print("five is greater than two")
+"""
+x=5
+x="hi there"
+print(x)
+
+y=str(3)
+print(type(y))
+y=int(3)
+print(y)
+
+y=float(3)
+print(y)
