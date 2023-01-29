a3ad15201da821d952ab54ba6457179f15b1a73e
Abdulrahman Alsuhaibi <109905084+a-alsuhaibi@users.noreply.github.com>
2023-01-29 10:54:32 +0300

    variables-and-other-basics

--- basics.py
+++ basics.py
@@ -1,15 +1,31 @@
-"""if 5>2 :
- print("five is greater than two")
-print("five is greater than two")
-"""
-x=5
-x="hi there"
-print(x)
+# """if 5>2 :
+#  print("five is greater than two")
+# print("five is greater than two")
+# """
+# x=5
+# x="hi there"
+# print(x)
 
-y=str(3)
-print(type(y))
-y=int(3)
-print(y)
+# y=str(3)
+# print(type(y))
+# y=int(3)
+# print(y)
 
-y=float(3)
-print(y)
+# y=float(3)
+# print(y)
+
+x, y, z = "orange", "banana", "apple"
+fruits=["banana","lemon","orange"]
+x,y,z=fruits
+x="python"
+y="is"
+z="awesome"
+print(x+' '+ y +' '+ z)
+#the best way to output values is to use the comma
+print(x,y,z)
+
+
+
+
+
+
