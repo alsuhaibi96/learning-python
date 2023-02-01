44f580b4bb0ac329ca93170e6c9070f9a7ef3922
Abdulrahman Alsuhaibi <61363696+alsuhaibi96@users.noreply.github.com>
2023-02-01 07:11:32 -0500

    string-basics

--- basics.py
+++ basics.py
@@ -1,5 +1,34 @@
+#This is multi strings in python
+x="""hello there
+I am using multi-strings in python"""
+
+#This is multi strings in python usign single quotation
+x='''hello there
+I am using multi-strings in python'''
+
+print(x)
 
 
 
+#loop throuhg banana word
+# for x in "banana":
+#  print(x)
+# check for a word
+
+#upper case
+x="hello there"
+print(x.upper())
+
+#lower case
+x="HELLO THERE"
+print(x.lower())
 
 
+#strip string
+x=" hello there  ."
+print(x.strip())
+
+#concatenate strings
+x="hello "
+y="there!!"
+print(x+" !" +y)
