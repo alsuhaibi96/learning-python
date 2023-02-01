216b6f57bd9e102ad18d8034657ac4f7c96a4a33
Abdulrahman Alsuhaibi <61363696+alsuhaibi96@users.noreply.github.com>
2023-02-01 13:50:18 -0500

    python lists basics

--- basics.py
+++ basics.py
@@ -1,31 +1,72 @@
-# """if 5>2 :
-#  print("five is greater than two")
-# print("five is greater than two")
-# """
-# x=5
-# x="hi there"
-# print(x)
+#Get the link of the list
 
-# y=str(3)
-# print(type(y))
-# y=int(3)
-# print(y)
-
-# y=float(3)
-# print(y)
-
-x, y, z = "orange", "banana", "apple"
-fruits=["banana","lemon","orange"]
-x,y,z=fruits
-x="python"
-y="is"
-z="awesome"
-print(x+' '+ y +' '+ z)
-#the best way to output values is to use the comma
-print(x,y,z)
+thelist = ["Apple", "Banana", "Orange"]
+print(len(thelist))
 
 
+#int , string, boolean types
+numbers=[1,50,30,20]
+stringValues=["car","motorcyle","bus"]
+booleanData=[True,False,True,False]
+
+print(booleanData)
+
+#Mixed data types
+differentDataType=["string",6,True]
+print(differentDataType)
 
 
+#Using list constructor to make a list
+newList=list(("hello","hi","there"))
+print(newList)
+
+#loop thought the items of list
+# for x in newList:
+#  print(x)
+
+#loop using index
+for i in range(len(newList)):
+ print(newList[i])
+
+ #while loop
+ print("while")
+ i=0
+ while(i<len(newList)):
+  print(newList[i])
+  i+=1
+
+#A short hand for loop that will print all items in a list:
+  thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+fruits =["Apple","lemon","banana"]
+#newlist = [expression for item in iterable if condition == True]
+newList=[x for x in fruits if "a" in x]
+
+print(newList)
 
 
+#The condition if x != "apple"  will return True for all elements other than "apple", making the new list contain all fruits except "apple".
+
+newlist = [x for x in fruits if x != "apple"]
+
+
+#Sort the list based on how close the number is to 50:
+
+def myfunc(n):
+  return abs(n - 50)
+
+listNumbers = [100, 50, 65, 82, 23]
+listNumbers.sort(key = myfunc)
+print(listNumbers)
+
+#Make a copy of a list with the copy() method:
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+
+#using extend to add to the end of the previous list
+thislist.extend(listNumbers)
+print(thislist) 
