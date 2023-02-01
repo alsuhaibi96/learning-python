190fa6c10d3acb13d4b7842de699b37c3d90f79b
Abdulrahman Alsuhaibi <61363696+alsuhaibi96@users.noreply.github.com>
2023-02-01 14:39:31 -0500

    dictionaries-basics

--- basics.py
+++ basics.py
@@ -29,3 +29,35 @@ print(x,y,z)
 
 
 
+"""Dictionary
+Dictionaries are used to store data values in key:value pairs.
+
+A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+
+As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.
+
+Dictionaries are written with curly brackets, and have keys and values:
+
+"""
+
+dictionary={
+    "name":"Mohammed",
+    "id":1,
+    "id":1
+}
+#Doesn't allow duplication
+print(dictionary)
+
+
+#the del keyword can also delete the dictionary completely:
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# del thisdict
+# print(thisdict)
+ #this will cause an error because "thisdict" no longer exists.
+
+ #items()	Returns a list containing a tuple for each key value pair
