c387bf63c469f6139bc12d0d9fe5a5134bf06bcb
Abdulrahman Alsuhaibi <61363696+alsuhaibi96@users.noreply.github.com>
2023-02-01 12:40:22 -0500

    boolean data types

--- basics.py
+++ basics.py
@@ -4,7 +4,24 @@ print(10<9)
 
 
 
+#compare
+x=10
+y=11
+if(x>y):
+ print(x ,"is greater than" ,y)
+else:
+ print(x ,"is smaller than" ,y)
 
 
+
+#those return false
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
 
 
