e89dfdcadcdba3cf070f42721fb66e0bd92ef7f5
Abdulrahman Alsuhaibi <61363696+alsuhaibi96@users.noreply.github.com>
2023-02-01 14:03:38 -0500

    some-basics-of-tuple

--- basics.py
+++ basics.py
@@ -29,3 +29,36 @@ print(x,y,z)
 
 
 
+"""Tuple
+Tuples are used to store multiple items in a single variable.
+
+Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+"""
+
+#Tuple is used to a collection which is ordered and unchangable
+#Tuples are written with round brackets.
+thisTuple=("hello","there")
+print(thisTuple)
+
+
+#One item tuple, remember the comma:
+
+
+#In Python ! Is this tuple ? 
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+#Negative indexing means starting from the end of the tuple.
+
+#This example returns the items from index -4 (included) to index -1 (excluded)
+
+#Remember that the last item has the index -1,
+
