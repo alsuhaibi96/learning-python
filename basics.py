d867e0a4fc17ca874a879302a8e47aed937d73ca
Abdulrahman Alsuhaibi <61363696+alsuhaibi96@users.noreply.github.com>
2023-02-05 21:01:28 +0300

    save-parameter-changes

--- basics.py
+++ basics.py
@@ -1,3 +1,17 @@
+#Swapping two values without using a third parameter
+a=20
+b=10
+
+print("values before swapping","a=",a, "b=",b)
+a=a+b #30
+b=a-b #20
+a=a-b #10
+
+print("values after swapping ")
+print("a=",a ,"b=",b )
+
+
+
 # """if 5>2 :
 #  print("five is greater than two")
 # print("five is greater than two")
@@ -20,12 +34,9 @@ x,y,z=fruits
 x="python"
 y="is"
 z="awesome"
-print(x+' '+ y +' '+ z)
+# print(x+' '+ y +' '+ z)
 #the best way to output values is to use the comma
-print(x,y,z)
-
-
-
+# print(x,y,z)
 
 
 
